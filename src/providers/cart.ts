bad0a09be721b2fb4c4f41f8fa68f5a9eac630c1
square1grp <developer01@square1grp.com>
2017-07-19 00:46:14 +0300

    Fix of the "0 or space" issue in Qty input field of Product Detail page.

--- src/providers/cart.ts
+++ src/providers/cart.ts
@@ -105,134 +105,127 @@ export class CartProvider {
   
   addToCart(product_title,colour,material,swatch,image,designer_title, designer_id, product_id, variant_id, size, size_id, type, qty, price, prodsku:string) {
 
-    
-    
-    console.log('Start:Adding to cart')
+      console.log('Start:Adding to cart')
 
-    //new order, set buyer_id
-    if (this.values.cart.request.order[0].buyer_id==0){
-      this.values.cart.request.device_token = this.values.user_profile.device_token;
-      this.values.cart.request.user_token = this.values.user_profile.user_token;
-      this.values.cart.request.order[0].buyer_id =this.values.user_profile.buyer_id; 
-      this.values.cart.request.order[0].user_id =this.values.user_profile.user_id;
-      this.values.cart.request.order[0].masquerade_originator = this.values.user_profile.masquerade_id;      
-    }
-
-    console.log('Check if designer is in cart')
-    //check for designer in cart
-    this.order_part_item_id=-1;
-    console.log('Cart Parts:'+JSON.stringify(this.values.cart.request.order[0].sales_order_parts))
-    let abort=false;
-    for (let pindex = 0, len = this.values.cart.request.order[0].sales_order_parts.length; pindex < len && !abort; pindex++) {  
-      if (this.values.cart.request.order[0].sales_order_parts[pindex].seller_account_id == designer_id) {
-        this.order_part_item_id = pindex;
-        abort=true;
+      //new order, set buyer_id
+      if (this.values.cart.request.order[0].buyer_id==0){
+          this.values.cart.request.device_token = this.values.user_profile.device_token;
+          this.values.cart.request.user_token = this.values.user_profile.user_token;
+          this.values.cart.request.order[0].buyer_id =this.values.user_profile.buyer_id; 
+          this.values.cart.request.order[0].user_id =this.values.user_profile.user_id;
+          this.values.cart.request.order[0].masquerade_originator = this.values.user_profile.masquerade_id;      
       }
-    };
 
-    //add sales_order_line
-    qty = parseInt(qty)
-    if (isNaN(parseFloat(qty))||(qty==undefined)){
-      qty=0
-    }
-
-    if(qty!=undefined){
-      this.line_item = {'product_title':product_title,'product_id':product_id, 'variant_id':variant_id,'variant_size_id':size_id, 'size_title':size, 'quantity':qty, 'price': price, 'sku':prodsku,'material':material,'colour':colour,'swatch':swatch,'image':image}
-      let hasVariant = 0
-      this.line_item_id=-1;
-      if (this.order_part_item_id!=-1){
-        let abort=false;
-        for (let lindex = 0, len = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines.length; lindex < len && !abort; lindex++) {   
-          if (this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[lindex].variant_id == this.line_item.variant_id) {
-            hasVariant = hasVariant+1;
-          }          
-          if (this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[lindex].sku == this.line_item.sku) {
-           this.line_item_id = lindex;
-           if(qty>0){abort=true}
+      console.log('Check if designer is in cart')
+      //check for designer in cart
+      this.order_part_item_id=-1;
+      console.log('Cart Parts:'+JSON.stringify(this.values.cart.request.order[0].sales_order_parts))
+      let abort=false;
+      for (let pindex = 0, len = this.values.cart.request.order[0].sales_order_parts.length; pindex < len && !abort; pindex++) {  
+          if (this.values.cart.request.order[0].sales_order_parts[pindex].seller_account_id == designer_id) {
+              this.order_part_item_id = pindex;
+              abort=true;
           }
-        };
+      };
+
+      //add sales_order_line
+      qty = parseInt(qty)
+      if (isNaN(parseFloat(qty))||(qty==undefined)){
+          qty=0;
       }
-      //  add new line item or replace QTY
-      let oldQty = 0
-      let doTally = 0
-      //new line item
-      if (this.line_item_id==-1){
-        
-        //  if a new designer (seller_account_id) then add sales_order_parts
-        if(qty>0||type=='all'){
-          if (this.order_part_item_id==-1){
-            this.line_total = price * qty;
-            this.order_part = {'designer_title': designer_title, 'seller_account_id':designer_id, 'buyer_id':this.values.user_profile.buyer_id, 'status':'REQUEST', 'commission_rate':'0.00', 'currency_code':'', 'total_line_items':0, 'total_amount':'0.00', 'total_qty':0, 'all_products':'','sales_order_lines':[this.line_item]};
-            this.order_part_item_id = this.values.cart.request.order[0].sales_order_parts.push(this.order_part) - 1;
-            console.log('New Order Part Index:'+this.order_part_item_id);
+
+      if(qty!=undefined){
+          this.line_item = {'product_title':product_title,'product_id':product_id, 'variant_id':variant_id,'variant_size_id':size_id, 'size_title':size, 'quantity':qty, 'price': price, 'sku':prodsku,'material':material,'colour':colour,'swatch':swatch,'image':image}
+          let hasVariant = 0;
+          this.line_item_id=-1;
+
+          if (this.order_part_item_id!=-1){
+              let abort=false;
+              for (let lindex = 0, len = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines.length; lindex < len && !abort; lindex++) {   
+                  if (this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[lindex].variant_id == this.line_item.variant_id) {
+                      hasVariant = hasVariant+1;
+                  }          
+                  if (this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[lindex].sku == this.line_item.sku) {
+                      this.line_item_id = lindex;
+                      if(qty>0){abort=true}
+                  }
+              };
+          }
+
+          //  add new line item or replace QTY
+          let oldQty = 0;
+          let doTally = 0;
+
+          //new line item
+          if (this.line_item_id==-1){
+            
+              //  if a new designer (seller_account_id) then add sales_order_parts
+              if(qty>=0||type=='all'){
+                  if (this.order_part_item_id==-1){
+                      this.line_total = price * qty;
+                      this.order_part = {'designer_title': designer_title, 'seller_account_id':designer_id, 'buyer_id':this.values.user_profile.buyer_id, 'status':'REQUEST', 'commission_rate':'0.00', 'currency_code':'', 'total_line_items':0, 'total_amount':'0.00', 'total_qty':0, 'all_products':'','sales_order_lines':[this.line_item]};
+                      this.order_part_item_id = this.values.cart.request.order[0].sales_order_parts.push(this.order_part) - 1;
+                      console.log('New Order Part Index:'+this.order_part_item_id);
+                  }
+                  else
+                  { 
+                      this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines.push(this.line_item);
+                  }
+                  this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_qty = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_qty + qty
+                  this.values.cart.request.order[0].total_qty = this.values.cart.request.order[0].total_qty + qty;
+                  if (hasVariant==0) {
+                      this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_line_items = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_line_items + 1
+                  }
+                  doTally=1;  
+              }
           }
           else
-          { 
-            this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines.push(this.line_item);
+          {
+              oldQty = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[this.line_item_id].quantity;
+              if (qty>=0||type=='all'){        
+                  //  recalc order part old qty
+                  doTally=1;
+                  this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[this.line_item_id].quantity = qty;
+                  this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_qty = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_qty + qty - oldQty
+                  //  recalc order part total amount / line items
+                  this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].line_total = ((qty * price) - (oldQty*price));
+                  if ((this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[this.line_item_id].quantity==0)&&(oldQty>0)){
+                      if(hasVariant==1){
+                        this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_line_items = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_line_items - 1
+                      }  
+                  }
+              }  
+              else
+              {
+                  //  remove the line item
+                  console.log("remove order line");
+                  //this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines.splice(this.line_item_id,1);
+              }
+
+              //recalc order total qty
+              this.values.cart.request.order[0].total_qty = this.values.cart.request.order[0].total_qty + qty - oldQty;
           }
-          this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_qty = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_qty + qty
-          this.values.cart.request.order[0].total_qty = this.values.cart.request.order[0].total_qty + qty
-          if (hasVariant==0) {
-            this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_line_items = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_line_items + 1
-          }
-          doTally=1;  
-        }
-      }
-      else
-      {
 
-        oldQty = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[this.line_item_id].quantity
-
-        if (qty>0||type=='all'){        
-
-          //  recalc order part old qty
-
-          doTally=1 
-
-          this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[this.line_item_id].quantity = qty  
-          this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_qty = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_qty + qty - oldQty
+          //  skip this if they tried to add new item with 0 QTY
           
-          //  recalc order part total amount / line items
-          
-          this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].line_total = ((qty * price) - (oldQty*price))
-          if ((this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines[this.line_item_id].quantity==0)&&(oldQty>0)){
-            if(hasVariant==1){
-              this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_line_items = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_line_items - 1
-            }  
+          if (doTally==1){
+              // recalc order part total
+              let OldTotal = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_amount;
+              let NewTotal = OldTotal - (oldQty*price) + (qty * price);
+              this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_amount = NewTotal;
           }
-
-        }  
-        else
-        {
-          //  remove the line item
-          console.log("remove order line");
-          //this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].sales_order_lines.splice(this.line_item_id,1);
-        }
-
-        //recalc order total qty
-        this.values.cart.request.order[0].total_qty = this.values.cart.request.order[0].total_qty + qty - oldQty
-      }
-
-      //  skip this if they tried to add new item with 0 QTY
       
-      if (doTally==1){
-        // recalc order part total
-        let OldTotal = this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_amount
-        let NewTotal = OldTotal - (oldQty*price) + (qty * price)
-        this.values.cart.request.order[0].sales_order_parts[this.order_part_item_id].total_amount = NewTotal
       }
-      
-    }
-    //console.log('Item Added:'+JSON.stringify(this.line_item));
-    //console.log('Cart Contains:'+JSON.stringify(this.values.cart));  
-    //console.log(this.line_item.sku)
+      //console.log('Item Added:'+JSON.stringify(this.line_item));
+      //console.log('Cart Contains:'+JSON.stringify(this.values.cart));  
+      //console.log(this.line_item.sku)
   }
 
   getSizeQty(sku, designer_id){
 
     //  get the QTY of the SKU in the order part for design_id
 
-    let qty=0
+    let qty=0;
     let abort = false;
     for (let i = 0, len = this.values.cart.request.order[0].sales_order_parts.length; i < len && !abort; i++) { 
       if (this.values.cart.request.order[0].sales_order_parts[i].seller_account_id == designer_id) {
@@ -262,7 +255,7 @@ export class CartProvider {
           console.log('Sales Order Line:'+j);
           if (this.values.vieworder.request.order[0].sales_order_parts[0].sales_order_lines[j].sku == sku) {
             qty = this.values.vieworder.request.order[0].sales_order_parts[0].sales_order_lines[j].quantity;
-            console.log('Found QTY in the cart for SKU:' + sku + ' of '+ qty )
+            console.log('Found QTY in the cart for SKU:' + sku + ' of '+ qty );
             abort=true;
             return qty;            
           }
